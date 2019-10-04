923ebee71a54790e4cffa9d336d09fdf57ebd7b6
Gustavo Petruff <gpetruff@gmail.com>
2019-10-04 15:21:58 -0300

    Update makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,10 +4,10 @@
 #TODO:Add a propertie to the file to make the creating of folders.
 
 function CreateSymLinks() {
-    cp -r -v i3/.i3status.conf ~/.i3status.conf;
-    cp -r -v i3/config ~/.i3/config;
-    cp -r -v polybar/config ~/.config/polybar/config;
-    cp -r -v polybar/launch.sh ~/.config/polybar/launch.sh;
+    rsync -aqz --progress i3/.i3status.conf ~/.i3status.conf;
+    rsync -aqz --progress i3/config ~/.i3/config;
+    rsync -aqz --progress polybar/config ~/.config/polybar/config;
+    rsync -aqz --progress polybar/launch.sh ~/.config/polybar/launch.sh;
 }
 
 CreateSymLinks
